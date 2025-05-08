6f539d68c521b3b47f6e473d1d5b3cc15793b27f
Shyunju <hyunju9908@naver.com>
2025-05-08 18:49:55 +0900

    20250508 마무리

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -76,7 +76,7 @@ public class GameManager : MonoBehaviour
     }
     private void Awake()
     {
-        //Screen.SetResolution(1080, 1920, true);
+        Screen.SetResolution(1080, 1920, true);
     }
     void Start()
     {   
