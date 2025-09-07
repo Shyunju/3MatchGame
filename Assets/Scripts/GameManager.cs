d6ea19233bfafee574c61877ad2d2bc802605923
Shyunju <hyunju9908@naver.com>
2025-09-07 22:50:49 +0900

    20250908코드정리

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,43 +18,42 @@ public class GameManager : MonoBehaviour
     public bool IsTuched {get{return isTuched;} set{isTuched = value;}}
     [SerializeField] float comboTime = 3.0f;
     public float ComboTime { get { return comboTime; } }
-    [SerializeField] AudioManager audioManager;
-    [SerializeField] GameObject selectLevelBoard;
-    [SerializeField] GameObject firstDisplay;
-    [SerializeField] TMP_Text timeTxt;                //시간
-    [SerializeField] TMP_Text scoreTxt;               //실시간 점수
-    [SerializeField] TMP_Text bestScoreTxt;           //최고점수
-    [SerializeField] TMP_Text number1Txt;             //피연산자1
-    [SerializeField] TMP_Text number2Txt;             //피연산자2
-    [SerializeField] TMP_Text operatorTxt;            //연산자
-    [SerializeField] TMP_Text answerTxt;              //비교될 값
-    [SerializeField] TMP_Text comparatorTxt;          //비교연산자
-    [SerializeField] TMP_Text currentScoreTxt;        //현재점수
-    [SerializeField] TMP_Text curGoalScoreTxt;        //현재 레벨 목표 점수
-    [SerializeField] GameObject gameOverBoard;        //제한시간이 끝난후
-    [SerializeField] GameObject gameStartBoard;       //게임 시작 전
-    [SerializeField] GameObject inGameCanvas;         //게임 진행 중
-    [SerializeField] GameObject pauseBoard;           //게임 일시정지
-    [SerializeField] GameObject protectBoard;
-    [SerializeField] private int score;
-    [SerializeField] Queue<int> numQueue = new Queue<int>();      //피연산자 큐
-    [SerializeField] private int successScore;
-    [SerializeField] private GameObject howToBoard;
-    [SerializeField] private GameObject bestImage;
-    [SerializeField] private Board board;
-    [SerializeField] private GameObject[] levelArr;
-    private int[] operatorArr = new int[2];             //연산자 배열
-    private int bestScore;
-    private int answer;
-    private bool isShowingHowToBoard = false;
-    private int[] goalScores = {0, 900, 900, 1500, 1800, 2000, 3000};
-    private int curLevel;
-    public int CurLevel {get{return curLevel;} set{curLevel = value;}}    
-    private int answerRangeMin = 3;
-    private int answerRangeMax;
-    private int operRange;
-    private int curGoalScore;
-    private int curNumberRange;
+    [SerializeField] AudioManager _audioManager;
+    [SerializeField] GameObject _selectLevelBoard;
+    [SerializeField] GameObject _firstDisplay;
+    [SerializeField] TMP_Text _timeTxt;                //시간
+    [SerializeField] TMP_Text _scoreTxt;               //실시간 점수
+    [SerializeField] TMP_Text _bestScoreTxt;           //최고점수
+    [SerializeField] TMP_Text _number1Txt;             //피연산자1
+    [SerializeField] TMP_Text _number2Txt;             //피연산자2
+    [SerializeField] TMP_Text _operatorTxt;            //연산자
+    [SerializeField] TMP_Text _answerTxt;              //비교될 값
+    [SerializeField] TMP_Text _comparatorTxt;          //비교연산자
+    [SerializeField] TMP_Text _currentScoreTxt;        //현재점수
+    [SerializeField] TMP_Text _curGoalScoreTxt;        //현재 레벨 목표 점수
+    [SerializeField] GameObject _gameOverBoard;        //제한시간이 끝난후
+    [SerializeField] GameObject _gameStartBoard;       //게임 시작 전
+    [SerializeField] GameObject _inGameCanvas;         //게임 진행 중
+    [SerializeField] GameObject _pauseBoard;           //게임 일시정지
+    [SerializeField] GameObject _protectBoard;
+    [SerializeField] int _score;
+    [SerializeField] Queue<int> _numQueue = new Queue<int>();      //피연산자 큐
+    [SerializeField] int _successScore;
+    [SerializeField] GameObject _howToBoard;
+    [SerializeField] GameObject _bestImage;
+    [SerializeField] Board _board;
+    [SerializeField] GameObject[] _levelArr;
+    private int[] _operatorArr = new int[2];             //연산자 배열
+    private int _answer;
+    private bool _isShowingHowToBoard = false;
+    private int[] _goalScores = {0, 900, 900, 1500, 1800, 2000, 3000};
+    private int _curLevel;
+    private int _answerRangeMin = 3;
+    private int _answerRangeMax;
+    private int _operRange;
+    private int _curGoalScore;
+    private int _curNumberRange;
+    public int CurLevel {get{return _curLevel;} set{_curLevel = value;}}    
 
 
     public enum QueueState
@@ -85,7 +84,7 @@ public class GameManager : MonoBehaviour
             PlayerPrefs.SetInt("ClearedLevel", 0);            
         }
         Time.timeScale = 1.0f; 
-        score = 0;
+        _score = 0;
     }
 
     void Update()
@@ -101,11 +100,11 @@ public class GameManager : MonoBehaviour
                 comboTime -= Time.deltaTime;
             }
             time -= Time.deltaTime;
-            timeTxt.text = time.ToString("N2");
-            scoreTxt.text = score.ToString();
+            _timeTxt.text = time.ToString("N2");
+            _scoreTxt.text = _score.ToString();
             if (time <= 0.0f)
             {
-                timeTxt.text = "0.00";
+                _timeTxt.text = "0.00";
                 isPlaying = false;
                 TimeIsUp();
             }
@@ -113,91 +112,91 @@ public class GameManager : MonoBehaviour
     }
     public void ChaingeScore(int num)
     {
-        score += num;
-        if(score < 0)
+        _score += num;
+        if(_score < 0)
         {
-            score = 0;
+            _score = 0;
         }
     }
     private void TimeIsUp() //제한시간 종료
     {
-        audioManager.StopBGM();
-        audioManager.PlayMatchedSound();
-        if(score >= curGoalScore){
-            audioManager.PlayNewRecordSound();
-            bestImage.SetActive(true);
+        _audioManager.StopBGM();
+        _audioManager.PlayMatchedSound();
+        if(_score >= _curGoalScore){
+            _audioManager.PlayNewRecordSound();
+            _bestImage.SetActive(true);
             int clearedLevels = PlayerPrefs.GetInt("ClearedLevels", 0);
-            clearedLevels |= 1 << curLevel % 10; //레벨 클리어
+            clearedLevels |= 1 << _curLevel % 10; //레벨 클리어
             PlayerPrefs.SetInt("ClearedLevels", clearedLevels);
             PlayerPrefs.Save();
         }
-        currentScoreTxt.text = score.ToString();
-        bestScoreTxt.text = curGoalScore.ToString();
-        gameOverBoard.SetActive(true);
+        _currentScoreTxt.text = _score.ToString();
+        _bestScoreTxt.text = _curGoalScore.ToString();
+        _gameOverBoard.SetActive(true);
         Time.timeScale = 0.0f;
-        board.currentState = GameState.wait;
+        _board.currentState = GameState.wait;
     }    
     public void GameStart()
     {
-        board.SettingPosition();
-        gameStartBoard.SetActive(false);
-        inGameCanvas.SetActive(true);
-        audioManager.GameMusicStart();
-        curGoalScoreTxt.text = "목표점수 : " + curGoalScore.ToString();
+        _board.SettingPosition();
+        _gameStartBoard.SetActive(false);
+        _inGameCanvas.SetActive(true);
+        _audioManager.GameMusicStart();
+        _curGoalScoreTxt.text = "목표점수 : " + _curGoalScore.ToString();
         MakeFormula();
     }
     private void MakeFormula()
     {
         //수연산자와 비교연산지 채우기
-        int oper = UnityEngine.Random.Range(1, operRange);
-        operatorArr[0] = oper;
+        int oper = UnityEngine.Random.Range(1, _operRange);
+        _operatorArr[0] = oper;
         switch(oper)
         {
             case 1:
-                operatorTxt.text = "+";
-                answerRangeMax = curNumberRange * 2 - 1;
+                _operatorTxt.text = "+";
+                _answerRangeMax = _curNumberRange * 2 - 1;
                 break;
             case 2:
-                operatorTxt.text = "-";
-                answerRangeMax = curNumberRange - 2;
+                _operatorTxt.text = "-";
+                _answerRangeMax = _curNumberRange - 2;
                 break;
             case 3:
-                operatorTxt.text = "x";
-                answerRangeMax = (int)Math.Pow(curNumberRange, 2) -1;
+                _operatorTxt.text = "x";
+                _answerRangeMax = (int)Math.Pow(_curNumberRange, 2) -1;
                 break;
             case 4:
-                operatorTxt.text = "%";
-                answerRangeMax = curNumberRange - 1;
+                _operatorTxt.text = "%";
+                _answerRangeMax = _curNumberRange - 1;
                 break;
         }
         int comp = UnityEngine.Random.Range(1, 4);
-        operatorArr[1] = comp;
+        _operatorArr[1] = comp;
         switch(comp)
         {
             case 1:
-                comparatorTxt.text = "<";
+                _comparatorTxt.text = "<";
                 break;
             case 2:
-                comparatorTxt.text = "<=";
+                _comparatorTxt.text = "<=";
                 break;
             case 3:
-                comparatorTxt.text = ">";
+                _comparatorTxt.text = ">";
                 break;
             case 4:
-                comparatorTxt.text = ">=";
+                _comparatorTxt.text = ">=";
                 break;
         }
-        answer = UnityEngine.Random.Range(answerRangeMin, answerRangeMax);
-        answerTxt.text = answer.ToString();
+        _answer = UnityEngine.Random.Range(_answerRangeMin, _answerRangeMax);
+        _answerTxt.text = _answer.ToString();
     }
     public void FillNumberText(int num)
     {
-        numQueue.Enqueue(num);
-        if(numQueue.Count == 1)
+        _numQueue.Enqueue(num);
+        if(_numQueue.Count == 1)
         {
-            number1Txt.text = num.ToString();
+            _number1Txt.text = num.ToString();
         }else{
-            number2Txt.text = num.ToString();
+            _number2Txt.text = num.ToString();
             curQState = QueueState.full;
             StartCoroutine(CheckFormulaCo());
         }
@@ -207,13 +206,13 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(.8f);
         if(CheckFormula())
         {
-            audioManager.PlayCorrectSound();
-            ChaingeScore(successScore);
+            _audioManager.PlayCorrectSound();
+            ChaingeScore(_successScore);
         }else{
-            audioManager.PlayWrongSound();
+            _audioManager.PlayWrongSound();
         }
-        number2Txt.text = "_";
-        number1Txt.text = "_";
+        _number2Txt.text = "_";
+        _number1Txt.text = "_";
         curQState = QueueState.empty;
         MakeFormula();
     }
@@ -222,9 +221,9 @@ public class GameManager : MonoBehaviour
     {
         //식 계산 확인하기 
         int result = 0;
-        int num1 = numQueue.Dequeue();
-        int num2 = numQueue.Dequeue();
-        switch(operatorArr[0])
+        int num1 = _numQueue.Dequeue();
+        int num2 = _numQueue.Dequeue();
+        switch(_operatorArr[0])
         {
             case 1:
                 result = num1 + num2;
@@ -239,19 +238,19 @@ public class GameManager : MonoBehaviour
                 result = num1 / num2;
                 break;
         }
-        switch(operatorArr[1])
+        switch(_operatorArr[1])
         {
             case 1:
-                if(result < answer)     return true;
+                if(result < _answer)     return true;
                 break;        
             case 2:
-                if(result <= answer)     return true;
+                if(result <= _answer)     return true;
                 break; 
             case 3:
-                if(result > answer)     return true;
+                if(result > _answer)     return true;
                 break; 
             case 4:
-                if(result >= answer)     return true;
+                if(result >= _answer)     return true;
                 break; 
             default: return false;
         }
@@ -262,40 +261,40 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         if(isPlaying){
-            board.currentState = GameState.wait;
+            _board.currentState = GameState.wait;
             isPlaying = false;
-            pauseBoard.SetActive(true);
-            audioManager.StopBGM();
+            _pauseBoard.SetActive(true);
+            _audioManager.StopBGM();
         }else{
-            board.currentState = GameState.move;
-            pauseBoard.SetActive(false);
+            _board.currentState = GameState.move;
+            _pauseBoard.SetActive(false);
             isPlaying = true;
-            audioManager.PlayBGM();
+            _audioManager.PlayBGM();
         }
 
     }
     public void ShowHowToBoard()
     {
-        if(isShowingHowToBoard)
+        if(_isShowingHowToBoard)
         {
-            isShowingHowToBoard = false;
+            _isShowingHowToBoard = false;
         }else{
-            isShowingHowToBoard = true;
+            _isShowingHowToBoard = true;
         }
-        howToBoard.SetActive(isShowingHowToBoard);
+        _howToBoard.SetActive(_isShowingHowToBoard);
     }
     public void SettingLevel(int level){
         CurLevel = level;
-        operRange = level > 200 ? 4 : 2;
-        curGoalScore = goalScores[level % 10];
-        curNumberRange = level % 100 / 10;
+        _operRange = level > 200 ? 4 : 2;
+        _curGoalScore = _goalScores[level % 10];
+        _curNumberRange = level % 100 / 10;
         time = level > 200 ? 90 : 60;
         GameStart();
     }
     public void GoToSelectLevel()
     {
-        firstDisplay.SetActive(false);
-        selectLevelBoard.SetActive(true);
+        _firstDisplay.SetActive(false);
+        _selectLevelBoard.SetActive(true);
         StartCoroutine(ProtectButtonCo());
         CheckLevelState();
     }
@@ -312,7 +311,7 @@ public class GameManager : MonoBehaviour
             if((clearedLevels & (1 << i)) == 0) //i레벨이 클리어 되어있지 않음
             {
                 //버튼 배열에서 아이번째 버튼 활성화
-                Button tempButton = levelArr[i].GetComponent<Button>();
+                Button tempButton = _levelArr[i].GetComponent<Button>();
                 tempButton.interactable = false;
             }
         }
@@ -321,7 +320,7 @@ public class GameManager : MonoBehaviour
     IEnumerator ProtectButtonCo()
     {
         yield return new WaitForSeconds(0.5f);
-        protectBoard.SetActive(false);
+        _protectBoard.SetActive(false);
     }
     public void GameExit()
 {
